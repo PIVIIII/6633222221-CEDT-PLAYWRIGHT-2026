75aca26b56228d81c1ab02fbb048b2504114f4f7
Variya Iyara <lenvariya@gmail.com>
2026-03-09 14:08:26 +0700

    edit Loginpage

--- page-objects/LoginPage.js
+++ page-objects/LoginPage.js
@@ -1,16 +1,14 @@
 export class LoginPage {
   constructor(page) {
     this.page = page;
-    this.url = process.env.URL || '';
-    console.log('URL:', this.url);
+    this.url = process.env.URL || 'https://katalon-demo-cura.herokuapp.com/';
   }
 
   async login(username, password) {
     await this.page.goto(this.url);
+
     await this.page.getByRole('link', { name: 'Make Appointment' }).click();
-    await this.page.getByLabel('Username').click();
     await this.page.getByLabel('Username').fill(username);
-    await this.page.getByLabel('Password').click();
     await this.page.getByLabel('Password').fill(password);
     await this.page.getByRole('button', { name: 'Login' }).click();
   }
