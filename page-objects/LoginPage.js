96941ae3f1404be079870c79eedb8ec68c5a0392
Variya Iyara <lenvariya@gmail.com>
2026-03-09 14:16:18 +0700

    edit Loginpage:username password2

--- page-objects/LoginPage.js
+++ page-objects/LoginPage.js
@@ -8,8 +8,8 @@ export class LoginPage {
     await this.page.goto(this.url);
 
     await this.page.getByRole('link', { name: 'Make Appointment' }).click();
-    await this.page.getByPlaceholder('Username').fill(username);
-    await this.page.getByPlaceholder('Password').fill(password);
+    await this.page.locator('#txt-username').fill(username);
+    await this.page.locator('#txt-password').fill(password);
     await this.page.getByRole('button', { name: 'Login' }).click();
   }
 }
