1b35758de239fedee1d2b15cc1d45055c15f8664
Variya Iyara <lenvariya@gmail.com>
2026-03-09 14:30:38 +0700

    edit var password

--- tests/EX03-pom.spec.ts
+++ tests/EX03-pom.spec.ts
@@ -6,10 +6,7 @@ import { AppointmentPage } from '../page-objects/AppointmentPage';
 test('Verify make appointment success', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
-  await loginPage.login(process.env.APP_USERNAME, process.env.PASSWORD);
-  console.log('process.env.APP_USERNAME', process.env.APP_USERNAME);
-  console.log('process.env.PASSWORD', process.env.PASSWORD);
-
+  await loginPage.login(process.env.APP_USERNAME, process.env.APP_PASSWORD);
   await expect(
     page.getByRole('heading', { name: 'Make Appointment' }),
   ).toBeVisible();
